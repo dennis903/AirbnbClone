31176167c44451959a66dc130df06cf0759a8635
이형진 <hyeolee@zempot.com>
2024-08-27 22:15:06 +0900

    jsconfig 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 import classnames from 'classnames/bind';
 import styles from './Header.module.css';
-import { AirBnbLogo } from '@/assets/img/airbnb.svg';
+import { ReactComponent as AirBnbLogo } from '@/assets/img/airbnb.svg';
 
 const cx = classnames.bind(styles);
 
 function Header() {
 	return (
 		<header>
-			<AirBnbLogo />
+			<AirBnbLogo width="120" height="100%" />
 		</header>
 	)
 }
--- src/view/pages/main/page.jsx
+++ src/view/pages/main/page.jsx
@@ -1,9 +1,10 @@
-import AppLayout from "../../layouts/app.layout";
+import AppLayout from '@/view/layouts/app.layout';
+import Header from '@/components/Header/Header';
 
 function MainPage() {
 	return (
 		<AppLayout>
-			<h1>Main Page</h1>
+			<Header />
 		</AppLayout>
 	)
 }
