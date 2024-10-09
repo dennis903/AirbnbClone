43fb067da1d83e4449c99a9c3774919aa83c541c
이형진 <hyeolee@zempot.com>
2024-10-09 20:10:29 +0900

    calendar 팝업 제작

--- src/components/SearchBar/SearchBarPopupCalendar/SearchBarPopupCalendar.jsx
+++ src/components/SearchBar/SearchBarPopupCalendar/SearchBarPopupCalendar.jsx
@@ -10,23 +10,58 @@ import './ReactDatePicker.custom.css';
 const cx = classNames.bind(styles);
 
 function SearchBarPopupCalendar() {
+	const [startDate, setStartDate] = useState(null);
+	const [endDate, setEndDate] = useState(null);
+	const [activeNav, setActiveNav] = useState({
+		date: true,
+		month: false,
+		flex: false
+	});
 
+	const handleChangeRange = (dates) => {
+		const [start, end] = dates;
+		setStartDate(start);
+		setEndDate(end);
+	}
+
+	const handleClickNav = (type) => {
+		setActiveNav({
+			date: type === 'date',
+			month: type === 'month',
+			flex: type === 'flex'
+		});
+	}
 
 	return (
 		<div className={cx('searchbar-popup')}>
 			<nav className={cx('searchbar-popup__nav')}>
 				<ul className={cx('searchbar-popup__nav-list')}>
-					<li className={cx('searchbar-popup__nav-item', 'searchbar-popup__nav-item--on')}>
+					<li
+						className={cx('searchbar-popup__nav-item', {
+							'searchbar-popup__nav-item--on': activeNav.date
+						})}
+						onClick={() => handleClickNav('date')}
+					>
 						<button type="button" className={cx('searchbar-popup__btn')}>
 							날짜 지정
 						</button>
 					</li>
-					<li className={cx('searchbar-popup__nav-item')}>
+					<li
+						className={cx('searchbar-popup__nav-item', {
+							'searchbar-popup__nav-item--on': activeNav.month
+						})}
+						onClick={() => handleClickNav('month')}
+					>
 						<button type="button" className={cx('searchbar-popup__btn')}>
 							월 단위
 						</button>
 					</li>
-					<li className={cx('searchbar-popup__nav-item')}>
+					<li
+						className={cx('searchbar-popup__nav-item', {
+							'searchbar-popup__nav-item--on': activeNav.flex
+						})}
+						onClick={() => handleClickNav('flex')}
+					>
 						<button type="button" className={cx('searchbar-popup__btn')}>
 							유연한 일정
 						</button>
@@ -34,16 +69,20 @@ function SearchBarPopupCalendar() {
 				</ul>
 			</nav>
 			<div className={cx('searchbar-popup__main')}>
-				<DatePicker
-					inline
-					selectsRange
-					monthsShown={2}
-					minDate={new Date()}
-					startDate={new Date()}
-					endDate={new Date().setDate(new Date().getDate() + 3)}
-					locale={ko}
-					dateFormatCalendar={'yyyy년 MM월'}
-				/>
+				<div className={cx('searchbar-calendar')}>
+					{activeNav.date && <DatePicker
+						inline
+						selectsRange
+						selected={startDate}
+						monthsShown={2}
+						minDate={new Date()}
+						startDate={startDate}
+						endDate={endDate}
+						locale={ko}
+						dateFormatCalendar={'yyyy년 MM월'}
+						onChange={handleChangeRange}
+					/>}
+				</div>
 			</div>
 		</div>
 	);
