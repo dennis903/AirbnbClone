1f560b690c952e995996cfa4d260b7226d39b94e
이형진 <hyeolee@zempot.com>
2024-10-23 21:49:01 +0900

    main content

--- src/view/pages/main/page.jsx
+++ src/view/pages/main/page.jsx
@@ -1,11 +1,13 @@
 import AppLayout from '@/view/layouts/app.layout';
 import Header from '@/components/Header/Header';
+import MainContent from '@/components/MainContent/MainContent';
 
 function MainPage() {
 	return (
 		<AppLayout>
 			<Header />
 			<main>
+				<MainContent />
 			</main>
 		</AppLayout>
 	)
--- src/view/pages/oauth/page.jsx
+++ src/view/pages/oauth/page.jsx
@@ -1,7 +1,15 @@
 import AppLayout from '@/view/layouts/app.layout';
 import Header from '@/components/Header/Header';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 function OauthPage() {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		navigate('/');
+	}, []);
+
 	return (
 		<AppLayout>
 			<Header />
