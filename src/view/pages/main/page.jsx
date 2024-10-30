6de646f10f2fac61d45118a4aca578a9bef1f5cd
juaovo <a01068020264@gmail.com>
2024-10-30 21:34:45 +0900

    메인 만드는중

--- src/view/pages/main/page.jsx
+++ src/view/pages/main/page.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import AppLayout from '@/view/layouts/app.layout';
 import Header from '@/components/Header/Header';
 import MainContent from '@/components/MainContent/MainContent';
+import Category from '@/components/Category/Category';
 
 import classNames from 'classnames/bind';
 import styles from './MainPage.module.css';
@@ -27,6 +28,7 @@ function MainPage() {
 		<AppLayout>
 			<Header />
 			<main>
+				<Category />
 				<div className={cx('contents')}>
 					{contents.map((content) => <MainContent key={content.id} content={content} />)}
 				</div>
